9f729e8b63145a152b9c727f415e10e0ced36658
matula.alien <matula@optonline.net>
2013-11-29 03:46:21 -0500

    step2 commiti 4th

--- step2.py
+++ step2.py
@@ -3,4 +3,7 @@ this is content for step2 , test for GIT
 this is a 2nd line, to test adding text
 
 
-this is a third line...
+this is a third line...   with a 4th modification
+
+checking when the updated occur in github server
+
